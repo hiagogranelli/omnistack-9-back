72d81df185a7bc9a63acf7437d75ec8198c3a200
unknown <hiagogr23@gmail.com>
2020-03-09 22:31:32 -0300

    feat: application

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@omnistack-oflqv.mongodb.net/semana09?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+// Metodos get, post, put, delete
+// req.query = acessar query params (para filtros)
+// req.params = acessar route params (para edição, delete)
+// req.body = acessar corpo da requisição (para criação e edição)
+
+app.use(cors());
+app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use(routes);
+
+app.listen(3333);
